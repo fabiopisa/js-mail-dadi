d1be5ce979ea0cea8be092e0b5ca5cb64039f023
fabiopisa <pisanellifabio@gmail.com>
2021-04-08 15:52:46 +0200

    commit esercizzi finiti completi

--- gioco dadi/assets/js/script.js
+++ gioco dadi/assets/js/script.js
@@ -7,9 +7,9 @@ var numero_computer = (Math.floor(Math.random()*7));
 console.log(numero_computer + " numero computer");
 
 if(numero_utente>numero_computer){
-  document.getElementById('risultato').innerHTML = "HAI VINTO!! <br> il tuo numero è " + numero_utente + " il numero del bot è " + numero_computer
-}else if(numero_utente=numero_computer){
-  document.getElementById('risultato').innerHTML = "HAI PAREGGIATO!! <br> il tuo numero è " + numero_utente + " il numero del bot è " + numero_computer
+  document.getElementById('risultato').innerHTML = "HAI VINTO!! <br> il tuo numero è " + numero_utente + " il numero del bot è " + numero_computer;
+}else if(numero_utente===numero_computer){
+  document.getElementById('risultato').innerHTML = "HAI PAREGGIATO!! <br> il tuo numero è " + numero_utente + " il numero del bot è " + numero_computer;
 }else{
-  document.getElementById('risultato').innerHTML = "HAI PERSO!! <br> il tuo numero è " + numero_utente + " il numero del bot è " + numero_computer
+  document.getElementById('risultato').innerHTML = "HAI PERSO!! <br> il tuo numero è " + numero_utente + " il numero del bot è " + numero_computer;
 }
